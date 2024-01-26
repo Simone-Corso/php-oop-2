30dd81861336f6805dd11b160785487a759fca27
Simone-Corso <scorso26@gmail.com>
2024-01-26 15:38:09 +0100

    ho appena inserito la prima classe per i prodotti

---
+++ Models/product.php
@@ -0,0 +1,16 @@
+<?php
+
+ /**
+*Dobbiamo immaginare quali classi necessarie per creare uno shop online per l'e-commerce che vende prodotti per gli animali.
+*farò la classe per i prodotti
+ */
+
+    class Prodotto{
+        public $productId;
+        public $title;
+        public $description;
+        public $price;
+        public $image;
+    }
+
+
