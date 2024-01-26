da2555c37c08c1ba5e2638a1b8e97e26386447ce
Simone-Corso <scorso26@gmail.com>
2024-01-26 15:53:00 +0100

    ho messo delle informazioni di quello che ho insertio

--- Models/article.php
+++ Models/article.php
@@ -1,10 +1,18 @@
 <?php
 
+/**
+ * prima classe per gli articoli
+ */
+
 class Article {
     public $articleId;
     public $type;
     public $productId;
     public $categoryId;
+
+/**
+ * ho inserito il construct
+ */
 
     public function __construct($articleId, $type, $productId, $categoryId) {
         $this->articleId = $articleId;
