cef4f070f1de9818b99c0e6b74bade4db5594038
Simone-Corso <scorso26@gmail.com>
2024-01-26 16:01:33 +0100

    ho inserito il primo cibo per i cani

--- Models/product.php
+++ Models/product.php
@@ -29,4 +29,14 @@
     
     }
 
+    /**
+     * Andrò a creare dei prodotti per i cani
+     */
+
+
+     /**
+      * Prodotti cibo per cani
+      */
+
+      $foodDogs= new Product (1, "Stuzzy Dog Monoprotein Lattina 400G" , "Cibo umido monoproteico per cani di Stuzzy, in gustioso patè con alta percentuale di materie prime, con pezzi selezionati. La ricetta monoproteica, grain free e gluten free, è caratterizzata da un'elevata digeribilità, priva di coloranti e conservanti aggiunti, adatta ai cani cui è stato prescritto un regime alimentare a eliminazione.", 2,30, "https://arcaplanet.vtexassets.com/arquivos/ids/257857/stuzzy-monoprotein-agnello-.jpg?v=637588282248800000")
 
