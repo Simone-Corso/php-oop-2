0ab70c407afc1f4dae724d366c96d40c8da6e6c7
Simone-Corso <scorso26@gmail.com>
2024-01-26 18:04:31 +0100

    devo finire di completare tutta la parte estetica

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ require_once __DIR__ . '/Models/product.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
     <title>Shop Online</title>
+    
 </head>
 <body>
 
