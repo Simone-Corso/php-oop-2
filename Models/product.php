390f4f5cb10aae124b5eb9e3c080aa974a398609
Simone-Corso <scorso26@gmail.com>
2024-01-28 23:21:51 +0100

    completato anche per la categoria gamescats

--- Models/product.php
+++ Models/product.php
@@ -88,20 +88,20 @@ public function getPrice(){
       * Prodotti cibo per gatti
       */
 
-      $foodCats= new Product (1, "Ket Cat Adult Bocconcini 720 gr" "Ket Cat Adult Bocconcini è l'alimento umido per gatti nelle pratiche lattine da 720 gr.", "1,90","c:\Users\hipho\AppData\Local\Temp\grafiche-magento-bauzaar_-_2023-09-27t122346.813.jpg");
+      $foodCats= new Product (1, "Ket Cat Adult Bocconcini 720 gr" "Ket Cat Adult Bocconcini è l'alimento umido per gatti nelle pratiche lattine da 720 gr.", "1,90","https://www.isoladeitesori.it/dw/image/v2/BGRZ_PRD/on/demandware.static/-/Sites-it-master-catalog/default/dw6e622f32/143643-lt-720gr-bocc.-pollo-e-tacchino.png?sw=800&sh=800");
       $foodCats= new Product (2, "Matisse Superpremium Cat Neutered Salmone " , "Matisse Sterilizzato al Salmone è un alimento completo studiato per le esigenze del vostro gatto sterilizzato. ", "9,25", "https://www.bauzaar.it/media/catalog/product/g/r/grafiche-magento-bauzaar_-_2023-08-02t165805.048.jpg?width=700&height=700&store=default&image-type=image");
-      $foodCats= new Product (3, "Stuzzy Cat Patè cotto al vapore per Gatti 100 gr - confezione da 32 " , "Stuzzy Cat Patè cotto al vapore per Gatti 100 gr è un alimento umido completo ad alta digeribilità per gatti adulti,", "16,64", "c:\Users\hipho\AppData\Local\Temp\grafiche-magento-bauzaar_-_2023-04-17t152258.766.jpg");
-      $foodCats= new Product (4, "Monge Gemon Cat Adult Bocconcini 415 gr." , "Monge Gemon Cat Bocconcini 415 gr è un cibo umido in lattina per gatti adulti disponibile in diverse ricette tutte completamente naturali e realizzate con carni italiane di altissima qualità. ", "24,90", "c:\Users\hipho\AppData\Local\Temp\grafiche-magento-bauzaar_-_2024-01-09t111241.613.jpg");
+      $foodCats= new Product (3, "Stuzzy Cat Patè cotto al vapore per Gatti 100 gr " , "Stuzzy Cat Patè cotto al vapore per Gatti 100 gr è un alimento umido completo ad alta digeribilità per gatti adulti,", "16,64", "https://egress.storeden.net/gallery/6123799c5fb8e0512818534b");
+      $foodCats= new Product (4, "Monge Gemon Cat Adult Bocconcini 415 gr." , "Monge Gemon Cat Bocconcini 415 gr è un cibo umido in lattina per gatti adulti disponibile in diverse ricette tutte completamente naturali e realizzate con carni italiane di altissima qualità. ", "24,90", "https://egress.storeden.net/jpg/5f90595ebe7ea02372aad40b");
 
 
    /**
       * Prodotti giochi per i gatti
       */
 
-      $gamesCats= new Product (1, "Gioco per Cane in Stoffa Peluche con Squittio" , "Peluche con squittio in stoffa di Trixie è l'accessorio perfetto per divertirti a lungo col tuo cane. ", 10,90 , "https://arcaplanet.vtexassets.com/arquivos/ids/218063/https---www.arcaplanet.it-media-catalog-product--t-r-trixie-cane-in-tessuto-peluche-tric78.jpg?v=637454594866330000");
-      $gamesCats= new Product (2, "Gioco Cane Peluche Zoo Elefante Arancio" , "Il Gioco Cane Zoo Elefante Arancio di Lovedì è un peluche per cvani con squittio per il divertimento quotidiano di Fido.  ", 5,90 , "https://arcaplanet.vtexassets.com/arquivos/ids/260727/lovedi-gioco-cane-peluche-zoo-elefante-arancio2.jpg?v=637618566736000000");
-      $gameaCats= new Product (3, "Gioco Cane Peluche Foodie Anguria" , "Il Peluche Foodie Anguria per cani di Lovedi con squittio è l'accessorio perfetto per divertirti a lungo con Fido.", 8,90, "https://arcaplanet.vtexassets.com/arquivos/ids/273081/LOVEDI-GIOCO-CANE-PELUCHE-FOODIE-ANGURIA.jpg?v=637920239238930000");
-      $gamesCats= new Product (4, "Gioco Cane Peluche Foodie Carota" , "Il Peluche Foodie Carota per cani di Lovedi con squittio è l'accessorio perfetto per divertirti a lungo con Fido.", 2,50 , "https://arcaplanet.vtexassets.com/arquivos/ids/273086/LOVEDI-GIOCO-CANE-PELUCHE-FOODIE-CAROTA.jpg?v=637920243838030000");
+      $gamesCats= new Product (1, "Cannetta gioco TIAKI Pesce con piume" , "Cannetta gioco per gatti, con ciondolo, spago in cotone, bastoncino liscio in vero legno, in 4 colori ", "3,49" , "https://shop-cdn-m.mediazs.com/bilder/met/prezzo/cannetta/gioco/tiaki/pesce/con/piume/6/400/351997_pla_fish_feather_cat_dagler_fg_0927_6.jpg");
+      $gamesCats= new Product (2, "Graffiatoio Onda" , "Graffiatoio per gatti, realizzato in cartone ondulato, riciclabile al 100%. Ideale per rifarsi le unghie, giocare o sonnecchiare!", "8,90" , "https://shop-cdn-m.mediazs.com/bilder/graffiatoio/onda/3/400/jan_mix_3_71_3.jpg");
+      $gameaCats= new Product (3, "Topino sonoro con catnip" , "Topino sonoro con catnip, in peluche e feltro rosso, con microchip integrato per effetti sonori realistici.", "5,78", "https://shop-cdn-m.mediazs.com/bilder/topino/sonoro/con/catnip/5/400/55405_PLA_Katzenspielzeug_Maus_mit_Katzenminze_und_Stimme_FG_DSC0500_5.jpg");
+      $gamesCats= new Product (4, "Gioco per gatti Felton" , "Gioco versatile Felton per gatti, con bastoncino in matatabi che stimola al gioco", "3,50" , "https://shop-cdn-m.mediazs.com/bilder/gioco/per/gatti/felton/2/400/197396_pla_katzenspielzeug_felton_fg_8212_2.jpg");
 
 
       /**
